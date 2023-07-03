17f13e59ef771ca4f0c3758b20ba75c60fe1f52d
mohamedsber <m.s.e12300@gmail.com>
2023-07-02 14:04:31 -1200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/screen/home_screen.dart';
-import 'package:food_delivery_app/screen/splash_screen.dart';
-import 'package:food_delivery_app/screen/widgets/custom_bottom_navi_bar.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,7 +11,6 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/screen/widgets/custom_bottom_navi_bar.dart
+++ lib/screen/widgets/custom_bottom_navi_bar.dart
@@ -1,6 +1,5 @@
 import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:food_delivery_app/screen/home_screen.dart';
 
 class CustomBottomNavigationBar extends StatefulWidget {
   const CustomBottomNavigationBar({Key? key}) : super(key: key);
